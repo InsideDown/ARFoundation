296788f5d60f25b11edef1c6318b23e068308062
Mealy, Paul <pmealy@fusionalliance.com>
2020-06-07 15:47:43 -0400

    - adding google VR; not working build but backup for changes

---
+++ Assets/Scripts/VR/VRGyroControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGyroControl : MonoBehaviour
+{
+
+
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private void Start()
+    {
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+
+        return false;
+    }
+}
