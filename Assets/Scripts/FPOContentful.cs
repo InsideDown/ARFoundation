7b84321f24b237aee24a57acf5a1bfd4318762fe
Mealy, Paul <pmealy@fusionalliance.com>
2020-06-18 18:09:22 -0400

    - adding contentful download code

--- Assets/Scripts/FPOContentful.cs
+++ Assets/Scripts/FPOContentful.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.Networking;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 [Serializable]
 public class RootJSON
@@ -18,6 +19,7 @@ public class RootJSON
     [Serializable]
     public class Field
     {
+        public string waypointId;
         public string title;
         public Content content;
     }
@@ -25,6 +27,7 @@ public class RootJSON
     [Serializable]
     public class Content
     {
+        [NonSerialized]
         public List<Content> content;
         public string nodeType;
         public string value;
@@ -36,39 +39,98 @@ public class RootJSON
     }
 }
 
+//contentful nodeTypes; document, paragraph, text
+
 public class FPOContentful : MonoBehaviour
 {
+    private const string URL_CONTENTFUL_ENTRIES = "https://cdn.contentful.com/spaces/gl7f1fzb2efs/environments/master/entries?access_token=1oBHjQBuqLUVnNI2SOZYlpBeFwDaWUIf51KJeHUWJRU";
+    private List<RootJSON.Item> JSONList;
+    private const string FILENAME = "waypoint-content.json";
+    private string LOCALPERSISTENTPATH ;
+
     // Start is called before the first frame update
     void Start()
     {
+        LOCALPERSISTENTPATH = Application.persistentDataPath + "/" + FILENAME;
         StartCoroutine(DownloadJSON());
+    }
+
+    /// <summary>
+    /// set our local file - always overwrite with whatever we pulled from the server
+    /// </summary>
+    /// <param name="resultBytes"></param>
+    private void SetLocalFile(byte[] resultBytes)
+    {
+        Debug.Log("setting local file");
+        File.WriteAllBytes(LOCALPERSISTENTPATH, resultBytes);
+    }
+
+    private void UseLocalFile()
+    {
+        if (!File.Exists(LOCALPERSISTENTPATH))
+        {
+            throw new System.Exception("The local file that you are trying to retrieve does not exist: " + LOCALPERSISTENTPATH);
+        }
+        else
+        {
+        
+            string jsonStr = File.ReadAllText(LOCALPERSISTENTPATH);
+
+
+            Debug.Log(LOCALPERSISTENTPATH);
+            Debug.Log("jsonStr");
+            Debug.Log(jsonStr);
+            ProcessJSON(jsonStr);
+        }
     }
 
     private IEnumerator DownloadJSON()
     {
-        UnityWebRequest www = UnityWebRequest.Get("https://cdn.contentful.com/spaces/gl7f1fzb2efs/environments/master/entries?access_token=1oBHjQBuqLUVnNI2SOZYlpBeFwDaWUIf51KJeHUWJRU");
+        UnityWebRequest www = UnityWebRequest.Get(URL_CONTENTFUL_ENTRIES);
         yield return www.SendWebRequest();
 
-        if (www.isNetworkError || www.isHttpError)
+        //if (www.isNetworkError || www.isHttpError)
+        if(!www.isNetworkError)
         {
             Debug.Log(www.error);
+            //see if we can pull our local file instead
+            UseLocalFile();
         }
         else
         {
             string jsonStr = www.downloadHandler.text;
-            // Show results as text
-            //Debug.Log(jsonStr);
-
-            // Or retrieve results as binary data
-            //byte[] results = www.downloadHandler.data;
-
-            //for (int i = 0; i < items.Items.Length; i++)
-            //{
-            //    Debug.Log(items.Items[i].Text);
-            //}
-            var MaxResponseObj = RootJSON.CreateFromJSON(jsonStr).items[0].fields.title;
-            Debug.Log(MaxResponseObj);
+            byte[] resultBytes = www.downloadHandler.data;
+            //set our local file with whatever we just downloaded
+            SetLocalFile(resultBytes);
+            ProcessJSON(jsonStr);
         }
+    }
+
+    private void ProcessJSON(string jsonStr)
+    {
+        JSONList = RootJSON.CreateFromJSON(jsonStr).items;
+        RootJSON.Field curField = GetWaypointInfo("waypoint2");
+        if (curField != null)
+        {
+            Debug.Log(curField.title);
+            Debug.Log(curField.content);
+        }
+    }
+
+    /// <summary>
+    /// based on a waypoint ID, pull our JSON info from a specific node
+    /// </summary>
+    /// <param name="waypointId"></param>
+    private RootJSON.Field GetWaypointInfo(string waypointId)
+    {
+        foreach(RootJSON.Item curItem in JSONList)
+        {
+            RootJSON.Field curField = curItem.fields;
+            if (waypointId == curField.waypointId)
+                return curField;
+        }
+        Debug.LogWarning("you are trying to access a waypointId which does not exist: " + waypointId);
+        return null;
     }
 
 
